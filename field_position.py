8d5d6292f521ca66145597d89408341100557b9e
Adam Lynch <adamclynch03@gmail.com>
2024-09-30 13:44:25 -0400

    refactor: organization

---
+++ field_position.py
@@ -0,0 +1,63 @@
+import nfldpw.pbp as pbp
+import nfldpw.pbp.cols as cols
+import matplotlib.pyplot as plt
+import seaborn
+import pandas
+
+
+CACHE = "cache/"
+
+
+def first_drive(df: pandas.DataFrame) -> pandas.DataFrame:
+    """
+    Get only plays that occur on 1st and 10 OR 1st and goal where the yards to the goaline is less than 10.
+    """
+    first_down = df[cols.Down.header] == 1
+    yards10 = df[cols.Ydstogo.header] == 10
+    and_goal = df[cols.Yardline100.header] == df[cols.Ydstogo.header]
+    yardsless10 = df[cols.Yardline100.header] <= 10
+    df = df[first_down & (yards10 | (and_goal & yardsless10))]
+    df = df[
+        [
+            cols.Yardline100.header,
+            cols.FixedDriveResult.header,
+        ]
+    ]
+    return df
+
+
+def compute_drive_points(df: pandas.DataFrame) -> pandas.DataFrame:
+    """
+    Compute the points scored on the given drive.
+
+    ```
+    = 7 * (FixedDriveResult == TOUCHDOWN)
+    + 3 * (FixedDriveResult == FIELD_GOAL)
+    - 2 * (FixedDriveResult == SAFETY)
+    - 7 * (FixedDriveResult == OPP_TOUCHDOWN)
+    ```
+
+    """
+    df["drive_points"] = (
+        7 * (df[cols.FixedDriveResult.header] == cols.FixedDriveResult.TOUCHDOWN)
+        + 3 * (df[cols.FixedDriveResult.header] == cols.FixedDriveResult.FIELD_GOAL)
+        - 2 * (df[cols.FixedDriveResult.header] == cols.FixedDriveResult.SAFETY)
+        - 7 * (df[cols.FixedDriveResult.header] == cols.FixedDriveResult.OPP_TOUCHDOWN)
+    )
+    return df[[cols.Yardline100.header, "drive_points"]]
+
+
+def plot_data(df: pandas.DataFrame):
+    seaborn.barplot(df, x=cols.Yardline100.header, y="drive_points", errorbar=None)
+    plt.title("Expected Drive Points by Starting Field Position")
+    plt.xlabel("Yards to Goal")
+    plt.ylabel("Expected Points")
+    plt.xticks(ticks=range(0, 100, 10), labels=range(0, 100, 10))
+    plt.savefig("figs/points_by_field_position")
+    plt.show()
+
+
+df = pbp.get([2021, 2022, 2023], CACHE)
+df = first_drive(df)
+df = compute_drive_points(df)
+plot_data(df)
