efb13252f37797945c83de95bc2d506a71ab77cd
Adam Lynch <adamclynch03@gmail.com>
2024-10-01 01:49:45 -0400

    fix: field goal with all years

--- field_goal.py
+++ field_goal.py
@@ -3,17 +3,49 @@ import nfldpw.pbp.cols as cols
 import matplotlib.pyplot as plt
 import seaborn
 import pandas
+from matplotlib.ticker import FuncFormatter
 
 
 CACHE = "cache/"
+YEARS = [
+    2002,
+    2003,
+    2004,
+    2005,
+    2006,
+    2007,
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,
+    2023,
+]
 
 
 def field_goals(df: pandas.DataFrame) -> pandas.DataFrame:
     fgs = df[cols.PlayType.header] == cols.PlayType.FIELD_GOAL
-    df = df[fgs]
     df["fg_made"] = df[cols.FieldGoalResult.header] == cols.FieldGoalResult.MADE
+    df = df[fgs]
     df = df[[cols.Yardline100.header, "fg_made"]]
     return df
+
+
+def formatter(x, tick):
+    if x % 10 == 0:
+        return f"{int(x)}"
+    else:
+        return ""
 
 
 def plot_data(df: pandas.DataFrame):
@@ -21,12 +53,11 @@ def plot_data(df: pandas.DataFrame):
     plt.title("Field Goal Probability by Yards")
     plt.xlabel("Yards to Goal")
     plt.ylabel("Field Goal Probability")
-    plt.xticks(ticks=range(0, 100, 10), labels=range(0, 100, 10))
-    plt.xlim(0, 50)
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(formatter))
     plt.savefig("figs/field_goal")
     plt.show()
 
 
-df = pbp.get([2021, 2022, 2023], CACHE)
+df = pbp.get(YEARS, CACHE)
 df = field_goals(df)
 plot_data(df)
